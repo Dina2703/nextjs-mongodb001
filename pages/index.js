a92ba7da42e09e82b350f0f6ec2f6fa7c2281052
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-15 10:58:42 -0400

    recieving the data into app from mongoDB,tested with console.log it works

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import "tailwindcss/tailwind.css";
 import Head from "next/head";
 import clientPromise from "../lib/mongodb";
 
-export default function Home({ isConnected }) {
+export default function Home({ movies }) {
+  console.log(movies);
   return (
     <div>
       <Head>
@@ -19,20 +20,17 @@ export default function Home({ isConnected }) {
 }
 
 export async function getServerSideProps(context) {
-  try {
-    // client.db() will be the default database passed in the MONGODB_URI
-    // You can change the database by calling the client.db() function and specifying a database like:
-    // const db = client.db("myDatabase");
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-    await clientPromise;
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
+  const client = await clientPromise;
+  const db = client.db("sample_mflix");
+  const data = await db.collection("movies").find({}).limit(20).toArray();
+  const movies = JSON.parse(JSON.stringify(data));
+  // client.db() will be the default database passed in the MONGODB_URI
+  // You can change the database by calling the client.db() function and specifying a database like:
+  // const db = client.db("myDatabase");
+  // Then you can execute queries against your database like so:
+  // db.find({}) or any of the MongoDB Node Driver commands
+
+  return {
+    props: { movies },
+  };
 }

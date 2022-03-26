e468fe2544973ba1b9570e05f872614f2e0d03b9
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-25 21:36:17 -0400

    minor changes

--- pages/[movie_id].js
+++ pages/[movie_id].js
@@ -8,24 +8,23 @@ export default function MovieDetails({ movie }) {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <>
-        {movie && (
-          <div className="container mx-auto">
+
+      {movie && (
+        <>
+          <div className="container mx-auto my-8">
             <h1 className="text-2xl">Movie Details For: {movie.title}</h1>
           </div>
-        )}
-      </>
-      <div className="container mx-auto">
-        <div className="flex flex-wrap my-8">
-          {movie && (
-            <div className="w-1/2 p-8 border border-black">
-              <img src={movie.poster} alt="movie poster" />
-              <p className="text-xs ">Release date: {movie.year}</p>
-              <p>IMDB Rating: {movie.imdb.rating}⭐️</p>
+          <div className="container mx-auto">
+            <div className="flex flex-wrap my-8">
+              <div className="w-1/2 p-8 border border-black">
+                <img src={movie.poster} alt="movie poster" />
+                <p className="text-xs ">Release date: {movie.year}</p>
+                <p>IMDB Rating: {movie.imdb.rating}⭐️</p>
+              </div>
             </div>
-          )}
-        </div>
-      </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
